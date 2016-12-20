2c70f59afe2acdc196decc913c558bd4381881f2
Brad Ziolko <t.m.m.xiv@gmail.com>
2016-12-20 14:19:13 -0600

    ArcadeRs 1.9, sdl_ttf was updated since this tutorial, struggling to adapt to the changes with sdl_ttf.

--- src/phi/mod.rs
+++ src/phi/mod.rs
@@ -3,7 +3,11 @@ mod events;
 pub mod data;
 pub mod gfx;
 
-use sdl2::render::Renderer;
+use self::gfx::Sprite;
+use sdl2::render::{Renderer, BlendMode};
+use sdl2::pixels::Color;
+use std::collections::HashMap;
+use std::path::Path;
 
 struct_events! {
     keyboard: {
@@ -21,20 +25,38 @@ struct_events! {
 
 pub struct Phi<'window> {
     pub events: Events,
-    pub renderer: Renderer<'window>
+    pub renderer: Renderer<'window>,
+
+    cached_fonts: HashMap<(&'static str, i32), ::sdl2_ttf::Font<'static>>
 }
 
 impl<'window> Phi<'window> {
     fn new(events: Events, renderer: Renderer<'window>) -> Phi<'window> {
         Phi {
             events: events,
-            renderer: renderer
+            renderer: renderer,
+            cached_fonts: HashMap::new()
         }
     }
 
     pub fn output_size(&self) -> (f64, f64) {
         let (w, h) = self.renderer.output_size().unwrap();
         (w as f64, h as f64)
+    }
+
+    pub fn ttf_str_sprite(&mut self, text: &str, font_path: &'static str, size: i32, color: Color) -> Option<Sprite> {
+        if let Some(font) = self.cached_fonts.get(&(font_path, size)) {
+            //return font.render(text).ok() //BlendMode::Blend).ok() //::sdl2_ttf::blended(color)).ok()
+            return font.render(text).blended(color).ok()
+                .and_then(|surface| self.renderer.create_texture_from_surface(&surface).ok())
+                .map(Sprite::new)
+        }
+        //::sdl2_ttf::Font::from_file(Path::new(font_path), size).ok()
+        ::sdl2_ttf::Sdl2TtfContext::load_font(Path::new(font_path), size as u16).ok()
+            .and_then(|font| {
+                self.cached_fonts.insert((font_path, size), font);
+                self.ttf_str_sprite(text, font_path, size, color)
+        })
     }
 }
 
---
+++ src/views/main_menu.rs
@@ -0,0 +1,95 @@
+use phi::{Phi, View, ViewAction};
+use phi::data::Rectangle;
+use phi::gfx::{CopySprite, Sprite};
+use sdl2::pixels::Color;
+
+
+struct Action {
+    func: Box<Fn(&mut Phi) -> ViewAction>,
+    idle_sprite: Sprite,
+    hover_sprite: Sprite
+}
+
+impl Action {
+    fn new(phi: &mut Phi, label: &'static str, func: Box<Fn(&mut Phi) -> ViewAction>) -> Action {
+        Action {
+            func: func,
+            idle_sprite: phi.ttf_str_sprite(label, "assets/belligerent.ttf", 32, Color::RGB(220, 220, 220)).unwrap(),
+            hover_sprite: phi.ttf_str_sprite(label, "assets/belligerent.ttf", 38, Color::RGB(255, 255, 255)).unwrap()
+        }
+    }
+}
+
+pub struct MainMenuView {
+    actions: Vec<Action>,
+    selected: i8
+}
+
+impl MainMenuView {
+    pub fn new(phi: &mut Phi) -> MainMenuView {
+        MainMenuView {
+            actions: vec![
+                Action::new(phi, "New Game", Box::new(|phi| {
+                    ViewAction::ChangeView(Box::new(::views::game::ShipView::new(phi)))
+                })),
+                Action::new(phi, "Quit", Box::new(|_| {
+                    ViewAction::Quit
+                }))
+            ],
+            selected: 0
+        }
+    }
+}
+
+impl View for MainMenuView {
+    fn render(&mut self, phi: &mut Phi, elapsed: f64) -> ViewAction {
+        if phi.events.now.quit || phi.events.now.key_escape == Some(true) {
+            return ViewAction::Quit;
+        }
+
+        if phi.events.now.key_space == Some(true) {
+            return (self.actions[self.selected as usize].func)(phi);
+        }
+
+        if phi.events.now.key_up == Some(true) {
+            self.selected -= 1;
+            if self.selected < 0 {
+                self.selected = self.actions.len() as i8 - 1;
+            }
+        }
+
+        if phi.events.now.key_down == Some(true) {
+            self.selected += 1;
+            if self.selected >= self.actions.len() as i8 {
+                self.selected = 0;
+            }
+        }
+
+        phi.renderer.set_draw_color(Color::RGB(0, 0, 0));
+        phi.renderer.clear();
+
+        let (win_w, win_h) = phi.output_size();
+
+        for (i, action) in self.actions.iter().enumerate() {
+            if self.selected as usize == i {
+                let (w, h) = action.hover_sprite.size();
+                phi.renderer.copy_sprite(&action.hover_sprite, Rectangle {
+                    x: (win_w - w) / 2.0,
+                    y: 32.0 + 48.0 * i as f64,
+                    w: w,
+                    h: h
+                });
+            } else {
+                let (w, h) = action.idle_sprite.size();
+                phi.renderer.copy_sprite(&action.idle_sprite, Rectangle {
+                    x: (win_w - w) / 2.0,
+                    y: 32.0 + 48.0 * i as f64,
+                    w: w,
+                    h: h
+                });
+            }
+        }
+
+        ViewAction::None
+    }
+}
